f4b8968693edbfeffd0ad1b7ab7f3377a828f046
Fixit-Davide <graziato.davide@outlook.it>
2024-10-26 15:13:43 +0200

    Added composition + mapping node

--- launch/drone_pose.launch.py
+++ launch/drone_pose.launch.py
@@ -18,6 +18,17 @@ def dump_params(path, name):
         return [yaml.safe_load(file)[name]['ros__parameters']]
 
 def generate_launch_description():
+
+  config = os.path.join(
+          get_package_share_directory('vdb_mapping_ros2'),
+          'config',
+          'vdb_params.yaml'
+          ),
+  bridge_config = os.path.join(
+          get_package_share_directory('drone_pose'),
+          'params',
+          'ros_bridge_config.yaml'
+          ),
   return LaunchDescription([
     # define static tf for base_link and camera.
     # Node(
@@ -28,22 +39,67 @@ def generate_launch_description():
     #     parameters=[{
     #       'use_sim_time': True}],   
     # ),
+    # define static tf for base_link and lidar.
+    Node(
+        package = "tf2_ros",
+        executable = "static_transform_publisher",
+        arguments = ["0.0", "0.0", "0.35", "0", "0", "0", "x500_lidar_0/base_link", "x500_lidar_0/link/lidar_2d_v2"],
+        output='screen',
+        parameters=[{
+          'use_sim_time': True}], 
+    ),
     # Node(
-    #     package = "tf2_ros",
-    #     executable = "static_transform_publisher",
-    #     arguments = ["0.0", "0.0", "0.35", "0", "0", "0", "x500_depth_0/base_link", "x500_depth_0/lidar_link/gpu_lidar"],
+    #     package='ros_gz_bridge',
+    #     executable='parameter_bridge',
+    #     # arguments=['/depth_camera@sensor_msgs/msg/Image@gz.msgs.Image',
+    #     #            '/depth_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'],
+    #     arguments=['/world/walls/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock',
+    #               '/world/walls/model/x500_lidar_0/link/link/sensor/lidar_2d_v2/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+    #               '/world/walls/model/x500_lidar_0/link/link/sensor/lidar_2d_v2/scan/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'],
     #     output='screen',
+    #     remappings=[('/world/walls/clock', '/clock')],
     #     parameters=[{
     #       'use_sim_time': True}], 
     # ),
-    Node(
-        package='drone_pose',
-        executable='DronePoseExecutable',
-        name='drone_pose',
-        output={
-                "stdout": "screen",
-                "stderr": "screen",
-        },
-        parameters=[os.path.join(get_package_share_directory("drone_pose"), 'params', 'drone_pose.yaml')],
+
+    ComposableNodeContainer(
+        name='Container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='vdb_mapping_ros2',
+                plugin='vdb_mapping_ros2::vdb_mapping_ros2_component',
+                name='vdb_mapping',
+                parameters=[config],
+                extra_arguments=[{'use_intra_process_comms': True}],
+            ),
+            ComposableNode(
+                package='drone_pose',
+                plugin='px4_autonav::DronePose',
+                name='drone_pose',
+                parameters=[os.path.join(get_package_share_directory("drone_pose"), 'params', 'drone_pose.yaml')],
+                extra_arguments=[{'use_intra_process_comms': True}],
+            ),
+            ComposableNode(
+                package='ros_gz_bridge',
+                plugin='ros_gz_bridge::RosGzBridge',
+                name='ros_gz_bridge',
+                # arguments=['/world/walls/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock',
+                #   '/world/walls/model/x500_lidar_0/link/link/sensor/lidar_2d_v2/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                #   '/world/walls/model/x500_lidar_0/link/link/sensor/lidar_2d_v2/scan/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'],
+                parameters=[{'config_file' : '/home/davide/ros2_drone_ws/src/PX4-ROS2-Drone-Composable/params/ros_bridge_config.yaml'}],
+                extra_arguments=[{'use_intra_process_comms': True}]
+            )
+        ],
+        output='screen',
     ),
+# , {'use_sim_time': True}
+    Node(
+       package='rviz2',
+       executable='rviz2',
+        parameters=[{
+          'use_sim_time': True}],
+    )
 ])
--- src/DronePose.cpp
+++ src/DronePose.cpp
@@ -16,42 +16,42 @@ Copyright 2023 Lance Drone Team
 // namespaces:
 using std::placeholders::_1;
 using namespace px4_msgs::msg;
-// using std::placeholders::_2;
 using namespace std::chrono;
 using namespace std::chrono_literals;
 
-namespace px4_autonav{
+namespace px4_autonav
+{
 
-  DronePose::DronePose(const rclcpp::NodeOptions & options)
-  : Node("drone_pose", options) {
-    // parameters:
-    base_frame = declare_parameter<std::string>("base_frame", "/map");
-    child_frame = declare_parameter<std::string>("child_frame", "/base_link");
-    pub_pose = declare_parameter<bool>("pub.pose", false);
-    pub_vel = declare_parameter<bool>("pub.vel", false);
-    pub_path = declare_parameter<bool>("pub.path", false);
-    // topics:
-    auto qos = rclcpp::QoS(rclcpp::KeepLast(5), rmw_qos_profile_sensor_data);
-    vehicle_odom_sub_ = create_subscription<VehicleOdometry>(
-                      "/fmu/out/vehicle_odometry",
-                      qos,
-                      std::bind(&DronePose::position_cb, this, _1));
-    vehicle_path_pub_ = create_publisher<nav_msgs::msg::Path>("/vehicle_path", 10);
-    vehicle_pose_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>("/vehicle_pose", 10);
-    vehicle_vel_pub_ = create_publisher<visualization_msgs::msg::Marker>("/vehicle_velocity", 10);
-    path_msg = std::make_shared<nav_msgs::msg::Path>();
-    tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
-    RCLCPP_INFO(this->get_logger(), "Init good.");
-  }
+DronePose::DronePose(const rclcpp::NodeOptions & options)
+: Node("drone_pose", options)
+{
+  // parameters:
+  base_frame = declare_parameter<std::string>("base_frame", "/map");
+  child_frame = declare_parameter<std::string>("child_frame", "/base_link");
+  pub_pose = declare_parameter<bool>("pub.pose", false);
+  pub_vel = declare_parameter<bool>("pub.vel", false);
+  pub_path = declare_parameter<bool>("pub.path", false);
+  // topics:
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(5), rmw_qos_profile_sensor_data);
+  vehicle_odom_sub_ = create_subscription<VehicleOdometry>(
+    "/fmu/out/vehicle_odometry",
+    qos,
+    std::bind(&DronePose::position_cb, this, _1));
+  vehicle_path_pub_ = create_publisher<nav_msgs::msg::Path>("/vehicle_path", 10);
+  vehicle_pose_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>("/vehicle_pose", 10);
+  vehicle_vel_pub_ = create_publisher<visualization_msgs::msg::Marker>("/vehicle_velocity", 10);
+  path_msg = std::make_shared<nav_msgs::msg::Path>();
+  tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+  RCLCPP_INFO(this->get_logger(), "Init good.");
+}
 
 void DronePose::position_cb(
-  const px4_msgs::msg::VehicleOdometry::SharedPtr odom_msg) 
+  const px4_msgs::msg::VehicleOdometry::SharedPtr odom_msg)
 {
-  steady_clock::time_point begin = steady_clock::now();
   geometry_msgs::msg::PoseStamped pose_msg;
   visualization_msgs::msg::Marker arrow_velocity_msg;
   rclcpp::Time time = get_clock()->now();
-  if(pub_pose){
+  if (pub_pose) {
     pose_msg.header.frame_id = base_frame;
     pose_msg.header.stamp = time;
     pose_msg.pose.orientation.w = odom_msg->q[0];
@@ -63,7 +63,7 @@ void DronePose::position_cb(
     pose_msg.pose.position.z = -odom_msg->position[2];
     vehicle_pose_pub_->publish(std::move(pose_msg));
   }
-  if(pub_vel){
+  if (pub_vel) {
     arrow_velocity_msg.action = 0;
     arrow_velocity_msg.header.frame_id = base_frame;
     arrow_velocity_msg.header.stamp = time;
@@ -87,7 +87,7 @@ void DronePose::position_cb(
     arrow_velocity_msg.points = {tail, head};
     vehicle_vel_pub_->publish(std::move(arrow_velocity_msg));
   }
-  if(pub_path){
+  if (pub_path) {
     path_msg->header = pose_msg.header;
     path_msg->poses.push_back(pose_msg);
     vehicle_path_pub_->publish(*path_msg);
@@ -106,10 +106,6 @@ void DronePose::position_cb(
   tf.transform.rotation.z = -odom_msg->q[3];
   tf.transform.rotation.w = odom_msg->q[0];
   tf_broadcaster_->sendTransform(tf);
-  steady_clock::time_point end = steady_clock::now();
-  // RCLCPP_INFO(
-  //   get_logger(), "[Time passed] = %ld [ms]",
-  //   duration_cast<milliseconds>(end - begin).count());
 }
 
 }  // px4_autonav
